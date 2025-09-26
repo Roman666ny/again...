5c7fb782e0e8876caa5ca92f390d0849f06a245b
Roman666ny <csalwa515@gmail.com>
2025-09-26 17:08:45 +0300

    Update min-heap.java

--- Java/min-heap.java
+++ Java/min-heap.java
@@ -1,8 +1,12 @@
 import java.util.PriorityQueue;
 
-PriorityQueue<Integer> heap = new PriorityQueue<>();
-heap.add(5);
-heap.add(2);
-heap.add(8);
-
-System.out.println(heap.poll());  // 2 (минимальный элемент)
+public class Main {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        heap.add(5);
+        heap.add(2);
+        heap.add(8);
+        
+        System.out.println(heap.poll());  // 2 (минимальный элемент)
+    }
+}
